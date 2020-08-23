e9765917c555f8d8c76ea3e58e93f64048dbac4d
Geoff Bruce-Payne <gbrucepayne@hotmail.com>
2020-08-23 17:14:53 -0400

    Expose commonMessageFormat decoder

--- src/infra/messageCodecs/index.js
+++ src/infra/messageCodecs/index.js
@@ -1,7 +1,14 @@
+/**
+ * Loader for database modules
+ */
 'use strict';
 
-const coreModem = require('./coreModem');
+const normalizedPath = require('path').join(__dirname);
+let modules = {};
 
-module.exports = {
-  coreModem,
-};
+require('fs').readdirSync(normalizedPath).forEach(file => {
+  let moduleName = file.split('.')[0];
+  modules[moduleName] = require(`./${moduleName}`);
+});
+
+module.exports = modules;
