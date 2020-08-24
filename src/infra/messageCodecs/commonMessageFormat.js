f102f001290e8bbbe727fc6faf7b1863dac3fd04
Geoff Bruce-Payne <gbrucepayne@hotmail.com>
2020-08-24 15:52:12 -0400

    renamed SIN/MIN

--- src/infra/messageCodecs/commonMessageFormat.js
+++ src/infra/messageCodecs/commonMessageFormat.js
@@ -3,6 +3,7 @@
 /**
  * Returns the interpreted value of the field based on dataType
  * @param {object} field A payload field { name, value, dataType }
+ * @returns {boolean|number|Buffer|Array|string} dependent on data type
  */
 function parseFieldValue(field) {
   if (!field.dataType) return field.stringValue;
@@ -33,4 +34,28 @@ function parseFieldValue(field) {
   }
 }
 
-module.exports = parseFieldValue;
+/**
+ * Parses a JSON payload, replacing string values with proper type
+ * @param {Object} message A ReturnMessage
+ * @returns {Object} parsed message
+ */
+function parse(message) {
+  if (!message.payloadJson) {
+    throw new Error(`Message ${message.messageId} has no JSON payload to parse`);
+  }
+  let parsedMessage = {
+    receivedTimeUtc: message.receivedTimeUtc,
+    codecServiceId: message.codecServiceId,
+    codecMessageId: message.codecMessageId,
+    name: message.name,
+  };
+  message.payloadJson.fields.forEach(field => {
+    parsedMessage[field.name] = parseFieldValue(field);
+  });
+  return parsedMessage;
+}
+
+module.exports = {
+  parseFieldValue,
+  parse,
+};
